97ec34db06ad446eb0172e3d704e0239071590ba
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-16 04:38:12 +0500

    feat: add GlobalSearch component

--- src/components/GlobalSearch.vue
+++ src/components/GlobalSearch.vue
@@ -1,11 +1,49 @@
 <template>
-  <article>
-    <form action="">
-      <input
-        type="text"
-        placeholder="Title, companies, expertise or benefits"
-      />
-      <button>Search</button>
+  <article class="global-search">
+    <form class="global-search-form" action="">
+      <label>
+        icon
+        <input
+          class="global-search-form__text"
+          type="text"
+          placeholder="Title, companies, expertise or benefits"
+        />
+      </label>
+      <button class="global-search-form__button">Search</button>
     </form>
   </article>
 </template>
+
+<script>
+export default {
+  name: "GlobalSearch",
+};
+</script>
+
+<style lang="scss" scoped>
+.global-search {
+  background: transparent url("../assets/images/backgroundImg.png") center
+    no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+
+  &-form {
+    background: #fff;
+    padding: 5px;
+    margin: 50px 0;
+    border-radius: 5px;
+
+    &__text {
+      width: 400px;
+      border: none;
+    }
+
+    &__button {
+      border: none;
+      padding: 10px 30px;
+      border-radius: 5px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Header></Header>
+    <GlobalSearch></GlobalSearch>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import GlobalSearch from "@/components/GlobalSearch.vue";
 
 export default {
   name: "Home",
   components: {
-    Header
-  }
+    Header,
+    GlobalSearch,
+  },
 };
 </script>
