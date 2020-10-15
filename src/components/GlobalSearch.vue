153d99b577e3a22f6799bf372ea423da7079466a
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-16 03:05:24 +0500

    feat: add basic components

---
+++ src/components/GlobalSearch.vue
@@ -0,0 +1,11 @@
+<template>
+  <article>
+    <form action="">
+      <input
+        type="text"
+        placeholder="Title, companies, expertise or benefits"
+      />
+      <button>Search</button>
+    </form>
+  </article>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1 class="logo_title"><span class="logo_title-bold">github</span> jobs</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="scss" scoped>
+.logo_title {
+  font: 400 24px "Poppins", sans-serif;
+  text-transform: capitalize;
+  color: #282538;
+  &-bold {
+    font-weight: 700;
+  }
+}
+</style>
