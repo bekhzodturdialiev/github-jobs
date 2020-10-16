1f9072a353f1dc343f52a44d120d208a056b1ced
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-16 19:59:40 +0500

    feat: add CheckBox component

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,50 @@
+<template>
+  <label class="search-criteria">
+    <input type="checkbox" class="search-criteria__checkbox" />
+    <span class="search-criteria__icon"></span>
+    <span class="search-criteria__title"><slot></slot></span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "CheckBox"
+};
+</script>
+
+<style lang="scss">
+.search-criteria {
+  display: flex;
+  align-items: center;
+  &__icon {
+    font-size: 1em;
+    display: inline-block;
+
+    &:after {
+      display: inline-block;
+      vertical-align: middle;
+      font-size: 1.5em;
+      font-family: "Material Icons";
+    }
+  }
+  &__checkbox {
+    display: none;
+  }
+  &__checkbox:not(:checked) ~ &__icon:after {
+    content: "\e835";
+    color: #b9bdcf;
+    margin: -3px;
+  }
+  &__checkbox:checked ~ &__icon:after {
+    content: "\e834";
+    color: #1e86ff;
+    margin: -3px;
+  }
+
+  &__title {
+    font: 500 0.875em "Poppins";
+    margin: 0 0 0 12px;
+    color: #334680;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <Header></Header>
     <GlobalSearch></GlobalSearch>
+    <CheckBox>Full stack</CheckBox>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
 import GlobalSearch from "@/components/GlobalSearch.vue";
+import CheckBox from "@/components/CheckBox.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
     GlobalSearch,
-  },
+    CheckBox
+  }
 };
 </script>
